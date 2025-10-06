40b7d5af78a42d632fb35351bbe9b9b9c6e7073f
Victor-Estrella <victorhcarracci@gmail.com>
2025-10-06 01:11:06 -0300

    Adiciona importação de PathVariable no UsuarioController para suporte a parâmetros de URL

--- src/main/java/br/com/fiap/dunoke/control/UsuarioController.java
+++ src/main/java/br/com/fiap/dunoke/control/UsuarioController.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.fiap.dunoke.model.Funcao;
